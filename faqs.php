a5eb39bffdeeaca46808c7aa9c94f41f286ebf9d
Preetu098 <preetu098@gmail.com>
2023-03-11 13:50:14 +0530

    added

---
+++ faqs.php
@@ -0,0 +1,88 @@
+<?php include('header.php');?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+<link href="assets/contact/style.css" rel="stylesheet"/>
+<!-- **Main** -->
+<div id="main">
+	<section class="main-title-section-wrapper aligncenter"
+		style="background:url(https://photographylife.com/wp-content/uploads/2016/06/Dragonfly.jpg) left top / auto repeat scroll;">
+		<div class="container">
+			<div class="main-title-section">
+				<h1>Faq'S</h1>
+			</div>
+			<div class="breadcrumb"><a href="https://dtdance.wpengine.com/">Home</a><span
+					class="fa default"></span><span class="current">Contact 2</span></div>
+		</div>
+	</section> <!-- ** Container ** -->
+	<div class="container">
+		<section id="primary" class="content-full-width"> <!-- #post-7819 -->
+			<div id="post-7819" class="post-7819 page type-page status-publish hentry">
+				<div class="vc_row wpb_row vc_row-fluid">
+					<div class="wpb_column vc_column_container vc_col-sm-12">
+						<div class="vc_column-inner ">
+			`				<div class="wpb_wrapper">
+								<div class="ult-animation  ult-animate-viewport  ult-no-mobile " data-animate="bounceIn"
+									data-animation-delay="0.5" data-animation-duration="1" data-animation-iteration="1"
+									style="opacity:0;" data-opacity_start_effect="">
+									<div class='dt-sc-title with-two-border with-sub-title aligncenter'>
+										
+										<h3>SoulFul Photography Academy</h3>
+										
+									</div>
+									<section1>
+        <div class="imgBx">
+            <img src="https://s.psecn.photoshelter.com/ps-inc/website/graphics/what-makes-us-unique-circle.png" alt="">
+        </div>
+		<br><br>
+        <div class="contentBx ">
+                            <h4 style="margin-top:-500px!important;">FAQs</h4>
+
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      Do I need to own a camera to join a course?
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">NO. Every student will be given a camera to use in the class.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+      Why are your fees so high? 
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Photography is an expensive hobby. Good quality camera gear costs lakhs of rupees. In order to allow our students to handle and experience high-end camera equipment of all brands requires a massive investment from our end. The value you get for your money is absolutely unparalleled, and if you are opting for a professional course, you can recover that money in no time. </div>
+    </div>
+  </div>
+ 
+</div>
+                    </div>
+    </section1>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				
+					</div>
+				</div>
+				<div class="vc_row-full-width vc_clearfix"></div>
+			</div><!-- #post-7819 -->
+		</section><!-- **Primary - End** -->
+	</div><!-- **Container - End** -->
+
+</div><!-- **Main - End** --> <!-- **Footer** -->
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<?php include('footer.php');?>
